3719b1734fa6c3e709b5ba587962c7a59e5cb105
taupirho <thomas.reid@gmail.com>
2018-06-13 16:56:24 +0100

    Update startStep.py

--- startStep.py
+++ startStep.py
@@ -2,7 +2,7 @@ import boto3
 def lambda_handler(event, context):
     client = boto3.client('stepfunctions')
     response = client.start_execution(
-        stateMachineArn='arn:aws:states:eu-west-2:696353118745:stateMachine:Mychoice',
+        stateMachineArn='arn:aws:states:eu-west-2:XXXXXXXXXXXXX:stateMachine:Mychoice',
         name="startStep",
         input="{\"bucket\":\"taupirho\",\"file\":\"iholding/issueholding.txt\",\"fsize\":\"0\"}"
         ) 
